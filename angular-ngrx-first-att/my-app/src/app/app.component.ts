f0c7bdcbf17f7a51dc496c44b5a3510d7f82df3e
Olha Pokolenko <olhp@ciklum.com>
2023-03-06 21:32:25 +0200

    all

---
+++ angular-ngrx-first-att/my-app/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {CountState} from "./reducers/count/count.reducer";
+import {Observable} from "rxjs";
+import {select, Store} from "@ngrx/store";
+import {selectCount, selectUpdatedAt} from "./reducers/count/count.selectors";
+import {
+  CountClearAction,
+  CountDecreaseAction,
+  CountIncreaseAction,
+  CountUpdatedAtAction
+} from "./reducers/count/count.actions";
+import {map} from "rxjs/operators";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: []
+})
+export class AppComponent {
+  public count$: Observable<number> = this.store$.pipe(select(selectCount));
+  public disableDecrease$: Observable<boolean> = this.count$.pipe(map(count => count <=0 ))
+  public updatedAt$: Observable<number> = this.store$.pipe(select(selectUpdatedAt));
+  constructor(private store$: Store<CountState>) {
+  }
+  increase() {
+    this.store$.dispatch(new CountIncreaseAction());
+  }
+  decrease() {
+    this.store$.dispatch(new CountDecreaseAction());
+  }
+  clear() {
+    this.store$.dispatch(new CountClearAction());
+  }
+}
