abe8f3e06ac8696d9fadf3215466ab749e68bdda
Olha Pokolenko <olhp@ciklum.com>
2023-03-11 17:03:18 +0200

    addedSortingOfContent

--- my-ngrx-course/src/app/courses/reducers/course.reducers.ts
+++ my-ngrx-course/src/app/courses/reducers/course.reducers.ts
@@ -1,15 +1,19 @@
-import {Course} from "../model/course";
+import {compareCourses, Course} from "../model/course";
 import {createEntityAdapter, EntityAdapter, EntityState} from "@ngrx/entity";
 import {createReducer, on} from "@ngrx/store";
 import {CourseActions} from "../action-types";
 
-export interface CoursesState extends EntityState<Course>{
+export interface CoursesState extends EntityState<Course> {
+
   // courses: Course[];
   // entities: {[key: number]: Course},
   // ids: number[]
-}
+};
 
-export const adapter: EntityAdapter<Course> = createEntityAdapter<Course>();
+export const adapter: EntityAdapter<Course> = createEntityAdapter<Course>({
+  sortComparer: compareCourses,
+  // selectId: course => course.id
+});
 
 export const initialCoursesState = adapter.getInitialState();
 
