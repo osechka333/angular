b3182c93c6fdef33c11547323cb06b7135737636
Olha Pokolenko <olhp@ciklum.com>
2023-03-15 21:57:20 +0200

    todo-list

---
+++ todo-app-angular-ngrx/src/app/modules/todo/ui/todo-create-form-ui/todo-create-form-ui.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-todo-create-form-ui',
+  templateUrl: './todo-create-form-ui.component.html',
+  styleUrls: ['./todo-create-form-ui.component.css']
+})
+export class TodoCreateFormUiComponent implements OnInit {
+  name = '';
+
+  @Output()
+  create = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onCreate() {
+    if (this.name) {
+      this.create.emit(this.name);
+      this.name = '';
+    }
+  }
+
+}
